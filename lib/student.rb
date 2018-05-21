d2ad19dc3947bda5485d5f36abeefe1f939d2b20
Felice Forby <feliceforby@gmail.com>
2018-05-21 02:16:46 +0000

    finish Student class methods

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,36 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize( student_hash )
+
+    # creates a self.attribute=(value) method for each data attribute passed in
+    # e.g. self.twitter = value
+    student_hash.each do |attribute, value|
+      self.send( ( "#{attribute}=" ), value )
+    end
+
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  # Create new students with data scraped by the Scraper
+  def self.create_from_collection( students_array )
+    students_array.each do |student|
+      self.new( student )
+    end
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  # Add additional attributes to a student using data scraped from the profile page
+  def add_student_attributes( attributes_hash )
+    attributes_hash.each do |attribute, value|
+      self.send( ( "#{attribute}=" ), value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
-
